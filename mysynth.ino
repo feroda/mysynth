9136a981087b234531c9e22fe64632ef9194317d
Luca Ferroni <luca@befair.it>
2018-02-21 22:45:32 +0100

    Aggiunge un pulsante per selezionare i led

--- mysynth.ino
+++ mysynth.ino
@@ -1,12 +1,21 @@
 const int LEDS_PIN[] = {6, 7};
+const int SWITCH_PIN = 3;
 
+int led_active = 0;
+int switch_value = 0;
+int old_switch_value = 0;
 
 void setup() {
-  
+  int led_init;
   Serial.begin(9600);
   
+  pinMode(SWITCH_PIN, INPUT);
   for (int i=0; i<=1; i++) {
-    pinMode(LEDS_PIN[i], OUTPUT);
+    led_init = LEDS_PIN[i];
+    pinMode(led_init, OUTPUT);
+    digitalWrite(led_init, HIGH);
+    delay(100);
+    digitalWrite(led_init, LOW);
   }
   
   Serial.println("setup() ends.");
@@ -16,12 +25,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int i=0; i< 2; i++) {
-    Serial.print("switch on led ");
-    Serial.println(LEDS_PIN[i]);
-    digitalWrite(LEDS_PIN[i], HIGH);
-    delay(200);
-    digitalWrite(LEDS_PIN[i], LOW);
+  switch_value = digitalRead(SWITCH_PIN);
+  Serial.print("button = ");
+  Serial.println(switch_value);
+
+  if (switch_value != old_switch_value) {
+
+      if (switch_value) {
+        digitalWrite(LEDS_PIN[led_active], LOW);
+        led_active = (led_active + 1) % 2;
+        Serial.print("led_active = ");
+        Serial.println(led_active);
+        digitalWrite(LEDS_PIN[led_active], HIGH);
+      }
+      old_switch_value = switch_value;
   }
 
   delay(1);
